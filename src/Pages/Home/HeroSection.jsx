6342c2c527b57c2383888823cfd9566d4fbd392e
iykecode93 <boscoiyke67@gmail.com>
2025-02-12 11:42:41 +0100

    update

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -5,7 +5,7 @@ import pics from "../../asset/john0 copy.png";
 const HeroSection = () => {
 
   const onButtonClick = () => {
-    fetch("JOHNBOSCO I. OPARAH S.Engr..pdf").then((response) => {
+    fetch("JOHNBOSCO IKECHUKWU OPARAH Software Engr..pdf").then((response) => {
       response.blob().then((blob) => {
         const fileURL =
             window.URL.createObjectURL(blob);
